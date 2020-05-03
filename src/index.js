cba3591c451edd050786541c707d0767c031a928
alberto diarian <alberto.diarian@unidadeditorial.es>
2020-05-03 20:25:34 +0200

    text styles

--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@
 // import './modulos/helpers/componente';
 import './scss/estilos.scss';
 
-let menuSidebar = document.querySelector('.menu-sidebar');
-let toogleBtn = document.querySelector('.menu-sidebar__button');
-let menuSidebarList = document.querySelector('.menu-sidebar__list')
+let menuSidebar = document.querySelector('.m-menu-sidebar');
+let toogleBtn = document.querySelector('.m-menu-sidebar__button');
+let menuSidebarList = document.querySelector('.m-menu-sidebar__list')
 let mainContainer = document.querySelector('.main-container');
 
 menuSidebarList.addEventListener('mouseover', ()=>{
-    mainContainer.classList.contains("menu-sidebar--closed") ? mainContainer.classList.remove("menu-sidebar--closed") : '';
+    mainContainer.classList.contains("m-menu-sidebar--closed") ? mainContainer.classList.remove("m-menu-sidebar--closed") : '';
 })
 menuSidebar.addEventListener('click', (e) => {
     console.log(e.target);
     
     let elemento = e.target;
     switch (true) {
-        case elemento.matches('.menu-sidebar__button , .menu-sidebar__button *'):
-            mainContainer.classList.toggle("menu-sidebar--closed");
+        case elemento.matches('.m-menu-sidebar__button , .m-menu-sidebar__button *'):
+            mainContainer.classList.toggle("m-menu-sidebar--closed");
             break;
-        case elemento.matches('.menu-sidebar__list-item-link'):
+        case elemento.matches('.m-menu-sidebar__list-item-link'):
             elemento.classList.toggle('link-selected');
             elemento.parentNode.classList.toggle('list-open');
             break;
